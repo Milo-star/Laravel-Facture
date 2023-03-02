2dd9cc8de0ef96640b8a399b8e3cb95674c30be6
Milo Bonnet <milovtt@gmail.com>
2023-03-02 11:32:39 +0100

    Front welcome.blade.php

--- storage/framework/views/19831a420984924187a2deff6be55430.php
+++ storage/framework/views/19831a420984924187a2deff6be55430.php
@@ -1,17 +1,22 @@
 <?php echo app('Illuminate\Foundation\Vite')('resources/css/app.css'); ?>
 <div> 
-    <h1 class="bg-red-600">Gestionnaire clients - factures</h1>
+    <h1 class="bg-red-800 p-40 text-4xl text-center text-white">Gestionnaire clients - factures</h1>
 </div>
-<div>
-    <div>
-        <h2>Clients</h2>
-        <a href="/clients/index">Afficher tous les client</a>
-        <a href="/clients/create">Créer un client</a>
+
+<div class="grid grid-cols-2">
+    <div class="p-4 text-center">
+        <h2 class="text-2xl p-3">Clients</h2>
+        <img src="https://www.citizencall.fr/wp-content/uploads/2018/11/Les-10-principes-de-la-relation-client.jpeg" alt="Les 10 principes de la relation client" class="my-2 w-1/2 mx-auto">
+        <a href="/clients/index" class="text-red-700 block p-3">Afficher tous les clients</a>
+        <a href="/clients/create" class="text-red-700 block">Créer un client</a>
     </div>
-    <div>
-        <h2>Factures</h2>
-        <a href="/factures/index">Afficher toutes les factures</a>
-        <a href="/factures/create">Créer une nouvelle factures</a>
+    <div class="p-4 text-center">
+        <h2 class="text-2xl p-3">Factures</h2>
+        <img src="https://www.netpme.fr/wp-content/uploads/2016/11/facture.jpg" alt="Liste des factures" class="my-2 w-1/2 mx-auto">
+        <a href="/clients/index" class="text-red-700 block p-3">Afficher toutes les factures</a>
+        <a href="/clients/create" class="text-red-700 block">Créer une facture</a>
     </div>
 </div>
+
+
 <?php /**PATH /home/becode/Laravel-Facture/resources/views/welcome.blade.php ENDPATH**/ ?>
