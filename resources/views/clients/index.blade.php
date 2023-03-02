69c8b7bbe41ee3faf0651c9a06abced6c072f02c
Milo Bonnet <milovtt@gmail.com>
2023-03-02 15:29:23 +0100

    FINISH

--- resources/views/clients/index.blade.php
+++ resources/views/clients/index.blade.php
@@ -1,36 +1,40 @@
-<h1>Gestionnaire de clients - factures</h1>
-<h2>Mes Clients</h2>
-<p>Dans cette section, vous pouvez voir la liste de tous vos clients</p>
+@vite('resources/css/app.css')
+<title>Gestionnaire clients - factures</title>
+<h1 class="bg-red-800 text-white justify-center p-40 text-center text-4xl">Gestionnaire clients - clients</h1>
+    <div class="mt-20 mb-4">
+        <h2 class="text-3xl mb-2 ml-20">Mes clients</h2>
+        <p class="ml-16 mb-10">Dans cette section, vous pouvez voir la liste de vos clients</p>
+    </div>
 
-<table>
+<table class="table-auto ml-16 w-3/4">
     <thead>
         <tr>
-            <th>id</th>
-            <th>Société</th>
-            <th>Téléphone</th>
-            <th>Email</th>
-            <th>Adresse</th>
-            <th>TVA</th>
-            <th>Factures</th>
-            <th>Opérations</th>
+            <th class="px-4 py-2">#</th>
+            <th class="px-4 py-2">Société</th>
+            <th class="px-4 py-2">Téléphone</th>
+            <th class="px-4 py-2">Email</th>
+            <th class="px-4 py-2">Adresse</th>
+            <th class="px-4 py-2">TVA</th>
+            <th class="px-4 py-2">Factures</th>
+            <th class="px-4 py-2">Opérations</th>
         </tr>
     </thead>
     <tbody>
         @foreach ($clients as $client)
         <tr>
-            <td>{{ $client->id }}</td>
-            <td>{{ $client->Company }}</td>
-            <td>{{ $client->phone }}</td>
-            <td>{{ $client->mail }}</td>
-            <td>{{ $client->adress }}</td>
-            <td>{{ $client->tva }}</td>
-            <td>{{ $client->Invoice }}</td>
-            <td><a href="{{ route('clients.edit', $client->id) }}">Modifier</a></td>
+            <td class="px-4 py-2 text-center">{{ $client->id }}</td>
+            <td class="px-4 py-2 text-center text-red-800">{{ $client->Company }}</td>
+            <td class="px-4 py-2 text-center">{{ $client->phone }}</td>
+            <td class="px-4 py-2 text-center">{{ $client->mail }}</td>
+            <td class="px-4 py-2 text-center">{{ $client->adress }}</td>
+            <td class="px-4 py-2 text-center">{{ $client->tva }}</td>
+            <td class="px-4 py-2 text-center text-red-800">{{ $client->Invoice }}</td>
+            <td class="px-4 py-2 text-center text-red-800"><a href="{{ route('clients.edit', $client->id) }}">Modifier</a></td>
             <td>
             <form action="{{ route('clients.destroy', $client->id) }}" method="POST">
                     @csrf
                     @method('DELETE')
-                    <button type="submit">Supprimer</button>
+                    <button type="submit" class="bg-red-500 text-white py-2 px-4 rounded">Supprimer</button>
                 </form>
             </td>
         </tr>
--- storage/framework/views/02a9eb70a73965017f4dcc17f33c92b6.php
+++ storage/framework/views/02a9eb70a73965017f4dcc17f33c92b6.php
@@ -1,6 +1,44 @@
-<?php $__env->startSection('content'); ?>
-    <div>
-        <h2>Clients</h2>
-        <a href="./clients">Afficher tous les clients</a>
-        <a href="./clients/create">Créer un client</a>
-    </div><?php /**PATH /home/becode/Laravel-Facture/resources/views/clients/index.blade.php ENDPATH**/ ?>
+<?php echo app('Illuminate\Foundation\Vite')('resources/css/app.css'); ?>
+<title>Gestionnaire clients - factures</title>
+<h1 class="bg-red-800 text-white justify-center p-40 text-center text-4xl">Gestionnaire clients - clients</h1>
+    <div class="mt-20 mb-4">
+        <h2 class="text-3xl mb-2 ml-20">Mes clients</h2>
+        <p class="ml-16 mb-10">Dans cette section, vous pouvez voir la liste de vos clients</p>
+    </div>
+
+<table class="table-auto ml-16 w-3/4">
+    <thead>
+        <tr>
+            <th class="px-4 py-2">#</th>
+            <th class="px-4 py-2">Société</th>
+            <th class="px-4 py-2">Téléphone</th>
+            <th class="px-4 py-2">Email</th>
+            <th class="px-4 py-2">Adresse</th>
+            <th class="px-4 py-2">TVA</th>
+            <th class="px-4 py-2">Factures</th>
+            <th class="px-4 py-2">Opérations</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php $__currentLoopData = $clients; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $client): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+        <tr>
+            <td class="px-4 py-2 text-center"><?php echo e($client->id); ?></td>
+            <td class="px-4 py-2 text-center text-red-800"><?php echo e($client->Company); ?></td>
+            <td class="px-4 py-2 text-center"><?php echo e($client->phone); ?></td>
+            <td class="px-4 py-2 text-center"><?php echo e($client->mail); ?></td>
+            <td class="px-4 py-2 text-center"><?php echo e($client->adress); ?></td>
+            <td class="px-4 py-2 text-center"><?php echo e($client->tva); ?></td>
+            <td class="px-4 py-2 text-center text-red-800"><?php echo e($client->Invoice); ?></td>
+            <td class="px-4 py-2 text-center text-red-800"><a href="<?php echo e(route('clients.edit', $client->id)); ?>">Modifier</a></td>
+            <td>
+            <form action="<?php echo e(route('clients.destroy', $client->id)); ?>" method="POST">
+                    <?php echo csrf_field(); ?>
+                    <?php echo method_field('DELETE'); ?>
+                    <button type="submit" class="bg-red-500 text-white py-2 px-4 rounded">Supprimer</button>
+                </form>
+            </td>
+        </tr>
+        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+    </tbody>
+</table>
+<?php /**PATH /home/becode/Laravel-Facture/resources/views/clients/index.blade.php ENDPATH**/ ?>
